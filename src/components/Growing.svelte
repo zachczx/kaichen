219d736cb232182e639891633c1f8286c0924df2
zachczx <zixian.chen@gmail.com>
2024-01-23 12:38:47 +0800

    edited

---
+++ src/components/Growing.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<span>
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mood-kid fill-orange-100 stroke-orange-500" width="300" height="300" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+        <path d="M9 10l.01 0" />
+        <path d="M15 10l.01 0" />
+        <path d="M9.5 15a3.5 3.5 0 0 0 5 0" />
+        <path d="M12 3a2 2 0 0 0 0 4" />
+    </svg>
+</span>
+<h1 class="mb-4">Hello! <span class="underline decoration-red-300 underline-offset-3">Kai's</span> a toddler.</h1>
--- src/components/Hello.svelte
+++ src/components/Hello.svelte
@@ -1,4 +1,3 @@
 <div class="container px-4 text-center">
-<h1 class="mb-4">Hello! This is <span class="underline decoration-red-300 underline-offset-3">Kai</span>.</h1>
-<p class="text text-3xl">Kai's just a kid.</p>
+    <h1 class="mb-4">Bye!</h1>
 </div>
